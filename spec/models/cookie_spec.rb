80c38959062fec35f4f829c7b266b4482bfc70de
Alexander Rowland <rowlanda@protonmail.com>
2021-03-21 19:11:29 -0700

    Refactor cookie_spec to use factory_bot factory

--- spec/models/cookie_spec.rb
+++ spec/models/cookie_spec.rb
@@ -3,24 +3,31 @@ require 'rails_helper'
 RSpec.describe Cookie, type: :model do
   context "#name" do
     it "prints the name of cookie" do
-      cookie = build(:cookie)
+      cookie = create(:cookie)
       expect(cookie.name).to eq("madeleine")
     end
   end
 
   context "validations" do
     it "ensures the presence of name" do
-      cookie = Cookie.new(name: "").save
-      expect(cookie).to eq(false)
+      # Builds a testable Cookie object without using factory_bot:
+      # cookie = Cookie.new(name: "").save
+      cookie = build(:cookie, name: "")
+      expect(cookie.save).to eq(false)
     end
 
     it "ensures that name has two or more characters" do
-      cookie = Cookie.new(name: "j").save
-      expect(cookie).to eq(false)
+      # Builds a testable Cookie object without using factory_bot:
+      # cookie = Cookie.new(name: "j").save
+      cookie = build(:cookie, name: "j")
+      expect(cookie.save).to eq(false)
     end
 
     it "ensures that valid name will be saved" do
-      cookie = Cookie.new(name: "macaroon")
+      # Builds a testable Cookie object without using factory_bot:
+      # cookie = Cookie.new(name: "macaroon")
+      cookie = build(:cookie, name: "macaroon")
+      expect(cookie.save).to eq(true)
     end
   end
 end
